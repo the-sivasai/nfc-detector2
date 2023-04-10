c1b25670250b071181f0ff7515bfd8a1220a29b7
Samba Siva Sai Davuluru <davulurusss@gmail.com>
2023-04-10 15:51:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
   const [isAccepting, setIsAccepting] = useState(false);
   const [isDeclining, setIsDeclining] = useState(false);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 10000);
+  }, []);
 
   const handleAccept = () => {
     setIsAccepting(true);
@@ -19,28 +24,28 @@ export default function Home() {
     setIsDeclining(true);
     setTimeout(() => {
       router.push("/decline");
-    }, 3000);
+    }, 9000);
   };
+  if (show)
+    return (
+      <div className="h-screen w-full flex items-center justify-center gap-6">
+        <p>ESV_1235 wants to connect</p>
+        <button
+          className="bg-green-500 text-white px-2 py-1 rounded"
+          onClick={handleAccept}
+          disabled={isAccepting || isDeclining}
+        >
+          {isAccepting ? "Accepting..." : "Accept"}
+        </button>
 
-  return (
-    <div className="h-screen w-full flex items-center justify-center gap-6">
-      <p>ESV_1235 wants to connect</p>
-
-      <button
-        className="bg-green-500 text-white px-2 py-1 rounded"
-        onClick={handleAccept}
-        disabled={isAccepting || isDeclining}
-      >
-        {isAccepting ? "Accepting..." : "Accept"}
-      </button>
-
-      <button
-        className="bg-red-500 text-white px-2 py-1 rounded"
-        onClick={handleDecline}
-        disabled={isDeclining || isAccepting}
-      >
-        {isDeclining ? "Declining..." : "Decline"}
-      </button>
-    </div>
-  );
+        <button
+          className="bg-red-500 text-white px-2 py-1 rounded"
+          onClick={handleDecline}
+          disabled={isDeclining || isAccepting}
+        >
+          {isDeclining ? "Declining..." : "Decline"}
+        </button>
+      </div>
+    );
+  return <p>Checking ...</p>;
 }
