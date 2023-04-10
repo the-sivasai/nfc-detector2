6875ee12e89d5a454eea3de0a9e5f4c8ec36fedb
Samba Siva Sai Davuluru <davulurusss@gmail.com>
2023-04-10 15:10:56 +0530

    test-2

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,45 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const [isAccepting, setIsAccepting] = useState(false);
+  const [isDeclining, setIsDeclining] = useState(false);
+
+  const handleAccept = () => {
+    setIsAccepting(true);
+    setTimeout(() => {
+      router.push("/accept");
+    }, 3000);
+  };
+
+  const handleDecline = () => {
+    setIsDeclining(true);
+    setTimeout(() => {
+      router.push("/decline");
+    }, 3000);
+  };
+
   return (
     <div className="h-screen w-full flex items-center justify-center gap-6">
       <p>ESV_1235 wants to connect</p>
+
       <button
         className="bg-green-500 text-white px-2 py-1 rounded"
-        onClick={() => router.push("/accept")}
+        onClick={handleAccept}
+        disabled={isAccepting || isDeclining}
       >
-        Accept
+        {isAccepting ? "Accepting..." : "Accept"}
       </button>
+
       <button
-        onClick={() => router.push("/decline")}
         className="bg-red-500 text-white px-2 py-1 rounded"
+        onClick={handleDecline}
+        disabled={isDeclining || isAccepting}
       >
-        Decline
+        {isDeclining ? "Declining..." : "Decline"}
       </button>
     </div>
   );
